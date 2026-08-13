4e3a7538c4bcbbb27a1043cd2998d5591abd358d
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2026-08-12 23:03:59 -0300

    add: textarea and regex

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { Header } from "./components/Header.jsx"
+import { useState } from "react";
+import { Header } from "./components/Header.jsx";
 
-function App() {
-   return (
-      <main>
-        <Header/>
-      </main>
-   )
-}
+const App = () => {
+  const [text, setText] = useState(
+    "Esto es un texto de prueba, puedes borrarlo, modificarlo o comprobar que ña app está funcionando correctamente",
+  );
+ const [excludeSpaces, setExcludeSpaces] = useState(false)
 
-export { App }
+ const characters = excludeSpaces ? text.replace(/\s/g, "").length : text.length
+
+  return (
+    <main>
+      <Header />
+      <h2>
+        Analyze you text <br />
+        in real-time.
+      </h2>
+      <textarea
+        placeholder="Escribe tu texto..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      ></textarea>
+      <div>
+        <label>
+          <input
+           type="checkbox" 
+           checked={excludeSpaces}
+          onChange={() => setExcludeSpaces(!excludeSpaces)}/>
+          Excluir espacios
+        </label>
+      </div>
+      <p>Cantidad de caracteres: {characters}</p>
+    </main>
+  );
+};
+
+export { App };
