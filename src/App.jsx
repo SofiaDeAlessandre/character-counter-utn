b3c726fcc925ec7dc8f5675a1854dd8046c07465
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2026-08-18 20:19:23 -0300

    add: main features: reading time

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,45 @@ import { Header } from "./components/Header.jsx";
 
 const App = () => {
   const [text, setText] = useState(
-    "Esto es un texto de prueba, puedes borrarlo, modificarlo o comprobar que ña app está funcionando correctamente",
+    "Esto es un texto de prueba, puedes borrarlo, modificarlo o comprobar que la app está funcionando correctamente",
   );
-  
- const [excludeSpaces, setExcludeSpaces] = useState(false)
- const [limitCharacter, setLimiterCharacter] = useState(false)
- const [limitValue, setLimitValue] = useState(10)
 
- const characters = excludeSpaces ? text.replace(/\s/g, "").length : text.length
+  const [excludeSpaces, setExcludeSpaces] = useState(false);
+  const [limitCharacter, setLimiterCharacter] = useState(false);
+  const [limitValue, setLimitValue] = useState(10);
 
- const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length //MEJORABLE, los espacios acá son tratados como caracteres !! 
+  const characters = excludeSpaces
+    ? text.replace(/\s/g, "").length
+    : text.length;
 
- const sentences = text.trim() === "" ? 0 : text.split(/[.!?]/).filter(sentence => sentence.trim() !== "").length
+  const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length; //MEJORABLE, los espacios acá son tratados como caracteres !!
 
- const handleChangeTextarea = (e) => {
-  const value = e.target.value
+  const sentences =
+    text.trim() === ""
+      ? 0
+      : text.split(/[.!?]/).filter((sentence) => sentence.trim() !== "").length;
 
-  if (limitCharacter) {
-if (value.length <= limitValue) {
-setText(value)
-}
-  } else {
-setText(value)
-  }
- }
+  const readingTime = Math.ceil(words / 200);
 
- const handleChangeInputLimit = () => {
-  setLimiterCharacter(!limitCharacter)
-  const newText = text.slice(0, limitValue)
-  setText(newText)
- }
+  const handleChangeTextarea = (e) => {
+    const value = e.target.value;
+
+    if (limitCharacter) {
+      if (value.length <= limitValue) {
+        setText(value);
+      }
+    } else {
+      setText(value);
+    }
+  };
+
+  const handleChangeInputLimit = () => {
+    setLimiterCharacter(!limitCharacter);
+    const newText = text.slice(0, limitValue);
+    setText(newText);
+  };
+
+  //FUNCIONALIDAD EXTRA: PODRIA HACERSE UN CHECKBOX PARA QUE OMITA O NO CARACTERES ESPECIALES / NUMEROS
 
   return (
     <main>
@@ -49,30 +58,32 @@ setText(value)
       <div>
         <label>
           <input
-           type="checkbox" 
-           checked={excludeSpaces}
-          onChange={() => setExcludeSpaces(!excludeSpaces)}/>
+            type="checkbox"
+            checked={excludeSpaces}
+            onChange={() => setExcludeSpaces(!excludeSpaces)}
+          />
           Excluir espacios
         </label>
-           <label>
+        <label>
           <input
-           type="checkbox" 
-           checked={limitCharacter}
-          onChange={handleChangeInputLimit}/>
+            type="checkbox"
+            checked={limitCharacter}
+            onChange={handleChangeInputLimit}
+          />
           Límite de caracteres
         </label>
-        {
-          limitCharacter &&
-           <input type="number"
-            value={limitValue} 
+        {limitCharacter && (
+          <input
+            type="number"
+            value={limitValue}
             onChange={(e) => setLimitValue(e.target.value)}
-            />
-        }
+          />
+        )}
       </div>
       <p>Cantidad de caracteres: {characters}</p>
       <p>Cantidad de palabras: {words}</p>
       <p>Cantidad de oraciones: {sentences}</p>
-      
+      <p>Tiempo aprox. de lectura: ~ {readingTime} min</p>
     </main>
   );
 };
