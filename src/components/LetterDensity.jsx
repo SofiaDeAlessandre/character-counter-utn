fda6fb69145e47544673e7c0d1b6c75ff9905b7d
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2026-08-19 22:15:05 -0300

    add: 'ProgressBar' component'

--- src/components/LetterDensity.jsx
+++ src/components/LetterDensity.jsx
@@ -1,25 +1,21 @@
-const LetterDensity = ({ visibleLetters, handleShowAll, showAll }) => {
-    return (
-    <section>
-        <h2>Cantidad de letras</h2>
-        <button onClick={() => handleShowAll(!showAll)}>
-          {showAll ? "Ver menos 🔼" : "Ver todos 🔽"}
-        </button>
-        <article>
-            <ul>
-          {visibleLetters.map((letter) => (
-            <li key={letter.letterName}>
-              <span>{letter.letterName.toUpperCase()}</span>
-              <meter min="0" max="100" value={letter.percentage}></meter>
-              <span>
-                {letter.amount} ({letter.percentage.toFixed(1)}%){" "}
-              </span>
-            </li>
-          ))}
-          </ul>
-        </article>
-      </section>
-      )
-}
+import { ProgressBar } from "./ProgressBar";
 
-export { LetterDensity }
+const LetterDensity = ({ visibleLetters, handleShowAll, showAll }) => {
+  return (
+    <section>
+      <h2>Cantidad de letras</h2>
+      <button onClick={() => handleShowAll(!showAll)}>
+        {showAll ? "Ver menos 🔼" : "Ver todos 🔽"}
+      </button>
+      <article>
+        <ul>
+          {visibleLetters.map((letter) => (
+            <ProgressBar key={letter.letterName} letter={letter} />
+          ))}
+        </ul>
+      </article>
+    </section>
+  );
+};
+
+export { LetterDensity };
